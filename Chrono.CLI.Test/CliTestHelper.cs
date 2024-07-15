0a77f05298bd724f922f5a80a1decdaab64c27f4
Ekkehard C. Damisch <yorschor@gmail.com>
2024-07-15 19:28:26 +0200

    test: Add Unit tests for VersionCommands

---
+++ Chrono.CLI.Test/CliTestHelper.cs
@@ -0,0 +1,117 @@
+using System.Diagnostics;
+using LibGit2Sharp;
+using Spectre.Console;
+using Spectre.Console.Testing;
+
+namespace Chrono.CLI.Test;
+
+public class CliTestHelper : IDisposable
+{
+    public readonly string TempDirectory;
+    private readonly CommandAppTester _fixture;
+
+    #region Setup
+
+    public CliTestHelper()
+    {
+        TempDirectory = CreateTempDirectory();
+        File.WriteAllText(Path.Combine(TempDirectory, "version.yml"), GetDefaultVersionFileContent());
+
+        Repository.Init(TempDirectory);
+        using (var repo = new Repository(TempDirectory))
+        {
+            LibGit2Sharp.Commands.Stage(repo, "version.yml");
+            repo.Commit("Initial commit", new Signature("Tester", "tester@example.com", DateTime.Now),
+                new Signature("Tester", "tester@example.com", DateTime.Now));
+            repo.Branches.Rename("master", "trunk");
+        }
+
+        // Set up the fixture
+        _fixture = new CommandAppTester();
+        _fixture.Configure(Chrono.GetAppConfigurator());
+    }
+
+    public void Dispose()
+    {
+        DeleteTempDirectory(TempDirectory);
+    }
+
+    #endregion
+
+    public void RunAndAssert(string[] command, string expectedOutput)
+    {
+        Directory.SetCurrentDirectory(TempDirectory);
+        var console = new TestConsole();
+        AnsiConsole.Console = console;
+        _fixture.Run(command);
+        if (!string.IsNullOrEmpty(expectedOutput))
+        {
+            Assert.Equivalent(expectedOutput, console.Output.Trim());
+        }
+
+        Debug.WriteLine($"Match: {string.Join(" ", command)} -> <{expectedOutput}>");
+    }
+
+    #region Internals
+
+    private static string GetDefaultVersionFileContent() => """
+                                                            ---
+                                                            version:
+                                                              1.0.0
+                                                            default:
+                                                              versionSchema: '{major}.{minor}.{patch}[-]{branch}[.]{commitShortHash}'
+                                                              precision: minor
+                                                              prereleaseTag: local
+                                                              release:
+                                                                match:
+                                                                  - ^v.*
+                                                                versionSchema: '{major}.{minor}.{patch}'
+                                                                precision:
+                                                                prereleaseTag:
+                                                            branches:
+                                                              release:
+                                                                match:
+                                                                  - ^release/.*
+                                                                versionSchema: '{major}.{minor}.{patch}-{prereleaseTag}-{commitShortHash}'
+                                                                precision: patch
+                                                                prereleaseTag: rc
+                                                            """;
+
+    private static string CreateTempDirectory()
+    {
+        var tempDirectory = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+
+        try
+        {
+            Directory.CreateDirectory(tempDirectory);
+            return tempDirectory;
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"An error occurred: {ex.Message}");
+            return "";
+        }
+    }
+
+    private static void DeleteTempDirectory(string tempDirectory)
+    {
+        try
+        {
+            if (Directory.Exists(tempDirectory))
+            {
+                Directory.Delete(tempDirectory, true);
+                Debug.WriteLine($"Temporary directory {tempDirectory} deleted successfully.");
+            }
+            else
+            {
+                Debug.WriteLine($"Directory {tempDirectory} does not exist.");
+            }
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"An error occurred while deleting the directory: {ex.Message}");
+        }
+    }
+
+    #endregion
+}
---
+++ Chrono.CLI.Test/VersionCommandTest.cs
@@ -0,0 +1,77 @@
+using System.Diagnostics;
+using LibGit2Sharp;
+
+// ReSharper disable InconsistentNaming
+
+namespace Chrono.CLI.Test;
+
+public class VersionCommandTest
+{
+    private readonly CliTestHelper App = new();
+
+    [Fact]
+    public void GetVersionCommandTest()
+    {
+        Debug.WriteLine("GetVersionCommandTest...");
+        using var repo = new Repository(App.TempDirectory);
+        var hash = repo.Head.Tip.Sha;
+        App.RunAndAssert(["get"], "1.0.0-trunk." + hash[..7]);
+    }
+    
+    [Fact]
+    public void GetVersionOnConfiguredBranchTest()
+    {
+        Debug.WriteLine("GetVersionOnConfiguredBranchTest...");
+        using var repo = new Repository(App.TempDirectory);
+        repo.CreateBranch("feature/test");
+        LibGit2Sharp.Commands.Checkout(repo, "feature/test");
+        var hash = repo.Head.Tip.Sha;
+        App.RunAndAssert(["get"], "1.0.0-feature-test." + hash[..7]);
+    }
+
+    [Fact]
+    public void GetVersionOnReleaseBranchTest()
+    {
+        Debug.WriteLine("GetVersionOnReleaseBranchTest...");
+        using var repo = new Repository(App.TempDirectory);
+        repo.CreateBranch("release/test");
+        LibGit2Sharp.Commands.Checkout(repo, "release/test");
+        var hash = repo.Head.Tip.Sha;
+        App.RunAndAssert(["get"], "1.0.0-rc-" + hash[..7]);
+    }
+    
+    [Fact]
+    public void GetVersionOnReleaseTagTest()
+    {
+        Debug.WriteLine("GetVersionOnReleaseTagTest...");
+        using var repo = new Repository(App.TempDirectory);
+        repo.ApplyTag("v1.0.0");
+        LibGit2Sharp.Commands.Checkout(repo, "v1.0.0");
+        App.RunAndAssert(["get"], "1.0.0");
+    }
+    [Fact]
+    public void SetVersionCommandTest()
+    {
+        Debug.WriteLine("SetVersionCommandTest...");
+        using var repo = new Repository(App.TempDirectory);
+        var hash = repo.Head.Tip.Sha;
+        
+        App.RunAndAssert(["set", "5.6.4"], "");
+        App.RunAndAssert(["get"], "5.6.4-trunk." + hash[..7]);
+    }
+    
+    [Fact]
+    public void BumpVersionCommandTest()
+    {
+        Debug.WriteLine("BumpVersionCommandTest...");
+        using var repo = new Repository(App.TempDirectory);
+        var hash = repo.Head.Tip.Sha;
+        App.RunAndAssert(["get"], "1.0.0-trunk." + hash[..7]);
+        App.RunAndAssert(["bump", "patch"], "");
+        App.RunAndAssert(["get"], "1.0.1-trunk." + hash[..7]);
+        App.RunAndAssert(["bump", "minor"], "");
+        App.RunAndAssert(["get"], "1.1.0-trunk." + hash[..7]);
+        App.RunAndAssert(["bump", "major"], "");
+        App.RunAndAssert(["get"], "2.0.0-trunk." + hash[..7]);
+    }
+}
