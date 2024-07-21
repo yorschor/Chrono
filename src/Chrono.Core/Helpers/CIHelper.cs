2e09fbb5467eb9318bc251cc4a39b125cbfc46c4
Ekkehard C. Damisch <yorschor@gmail.com>
2024-07-21 21:53:36 +0200

    feat: Add ci helper

---
+++ src/Chrono.Core/Helpers/CIHelper.cs
@@ -0,0 +1,34 @@
+namespace Chrono.Core.Helpers;
+
+public class CiHelper
+{
+    public static bool IsCiBuild()
+    {
+        return IsGitHubActions() || IsGitLabCi() || IsJenkinsCi() || IsAzurePipelines() || IsTeamCity();
+    }
+    
+    static bool IsGitLabCi()
+    {
+        return !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("GITLAB_CI"));
+    }
+    static bool IsJenkinsCi()
+
+    {
+        return !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("JENKINS_URL"));
+    }
+
+    static bool IsAzurePipelines()
+    {
+        return !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("TF_BUILD"));
+    }
+
+    static bool IsGitHubActions()
+    {
+        return !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("GITHUB_ACTIONS"));
+    }
+
+    static bool IsTeamCity()
+    {
+        return !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("TEAMCITY_VERSION"));
+    }
+}
