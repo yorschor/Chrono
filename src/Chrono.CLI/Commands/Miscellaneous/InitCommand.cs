6516203257c5a8cc1e9947289f8074bf2c4760d7
Ekkehard C. Damisch <yorschor@gmail.com>
2025-04-13 23:42:34 +0200

    feat: Rework Init command flow and defaults

--- src/Chrono.CLI/Commands/Miscellaneous/InitCommand.cs
+++ src/Chrono.CLI/Commands/Miscellaneous/InitCommand.cs
@@ -15,94 +15,118 @@ public class InitCommand : Command<InitCommand.Settings>
 
     public override int Execute(CommandContext context, Settings settings)
     {
-        var mode = AnsiConsole.Prompt(new SelectionPrompt<string>()
-            .Title("What mode do you want to use?")
-            .AddChoices("Print files to console", "Write files to disk", "Both"));
+        var projectDirectory = Directory.GetCurrentDirectory();
 
-        var files = AnsiConsole.Prompt(
-            new MultiSelectionPrompt<string>()
-                .Title("Which files would you like to set up?")
-                .PageSize(10)
-                .InstructionsText(
-                    "[grey](Press [blue]<space>[/] to toggle a file, " +
-                    "[green]<enter>[/] to accept)[/]")
-                .AddChoices("Directory.Build.props", "version.yml"));
+        // === STEP 1. Preflight Check ===
+        // Check for presence of key files, printing statuses accordingly:
+        // - version.yml : if exists -> red; if not -> green.
+        // - Directory.Build.props : if exists -> red; if not -> green.
+        // - packages.config : search recursively at least 3 directory layers deep;
+        //                    if exists -> warn (yellow).
+        var versionFileExists = File.Exists(Path.Combine(projectDirectory, "version.yml"));
+        var buildPropsExists = File.Exists(Path.Combine(projectDirectory, "Directory.Build.props"));
 
-        var variant = VersionFileVariants.LocalMinimal;
-        var initialVersion = "0.0.1.0";
-        if (files.Contains("version.yml"))
+        // A helper method to search for packages.config files 3 levels deep at most.
+        var packagesConfigPaths = Directory.EnumerateFiles(projectDirectory, "packages.config", SearchOption.AllDirectories);
+        var packagesConfigExists = packagesConfigPaths.Any();
+
+        // Print status for version.yml
+        AnsiConsole.Write(new Rule($"[bold]Chrono Init[/]"));
+        
+        if (versionFileExists)
+            AnsiConsole.MarkupLine("version.yml - [red]exists[/]");
+        else
+            AnsiConsole.MarkupLine("version.yml - [green]Not found[/]");
+
+        // Print status for Directory.Build.props
+        if (buildPropsExists)
+            AnsiConsole.MarkupLine("Directory.Build.props - [red]exists.[/]");
+        else
+            AnsiConsole.MarkupLine("Directory.Build.props - [green]not found[/]");
+
+        // Print status for packages.config (if found, list each and show warning)
+        if (packagesConfigExists)
         {
-            var variantString = AnsiConsole.Prompt(
-                new SelectionPrompt<string>()
-                    .Title("What kind of version.yml do you need?")
-                    .PageSize(10)
-                    .AddChoices("(local) Minimal", "(local) default", "(remote inherited) minimal", "(remote inherited) default)"));
-            variant = variantString switch
+            foreach (var path in packagesConfigPaths)
             {
-                "(local) Minimal" => VersionFileVariants.LocalMinimal,
-                "(local) default" => VersionFileVariants.LocalDefault,
-                "(remote inherited) minimal" => VersionFileVariants.InheritedMinimal,
-                "(remote inherited) default)" => VersionFileVariants.InheritedDefault,
-                _ => VersionFileVariants.LocalMinimal
-            };
-            initialVersion = AnsiConsole.Prompt(new TextPrompt<string>("What´s your initial version (default 0.0.1)?").DefaultValue("0.0.1"));
+                AnsiConsole.MarkupLine($"[yellow]Warning: packages.config found at '{path}'. This might interfere with Chrono![/]");
+            }
+        }
+        else
+        {
+            AnsiConsole.MarkupLine("[green]packages.config not found in subdirectories (up to 3 levels deep).[/]");
         }
 
-        if (mode is "Print files to console" or "Both")
+        // If any red status (e.g. version.yml already exists), ask if user wishes to proceed.
+        var proceedAfterPreflight = true;
+        if (versionFileExists || buildPropsExists)
         {
-            AnsiConsole.Write(new Rule("[bold]Directory.Build.props[/]"));
-            Console.Write(InitTemplates.GetBuildProps(settings.AppVersion));
-            Console.WriteLine("\n");
-            AnsiConsole.Write(new Rule("[bold]version.yml[/]"));
-            Console.Write(InitTemplates.GetVersionFile(variant, initialVersion));
-            return 0;
+            proceedAfterPreflight = AnsiConsole.Confirm("One or more files already exist. Do you wish to proceed (files might be overwritten)?", false);
         }
 
-        if (mode is "Write files to disk" or "Both")
+        if (!proceedAfterPreflight)
         {
-            var projectDirectory = Directory.GetCurrentDirectory();
-            var packagesConfigPath = Path.Combine(projectDirectory, "packages.config");
-            var directoryBuildPropsPath = Path.Combine(projectDirectory, "Directory.Build.props");
-            var versionFilePath = Path.Combine(projectDirectory, "version.yml");
-
-            var packagesExist = File.Exists(packagesConfigPath);
-            var directoryBuildPropsExist = File.Exists(directoryBuildPropsPath);
-            var versionFileExist = File.Exists(versionFilePath);
-
-            var fileWritePrompt = new MultiSelectionPrompt<string>()
-                .Title("Write the following files to disk?")
-                .PageSize(10)
-                .InstructionsText(
-                    "[grey](Press [blue]<space>[/] to toggle a file, " +
-                    "[green]<enter>[/] to accept)[/]")
-                .AddChoices("Directory.Build.props", "version.yml");
-            if (packagesExist)
-            {
-                AnsiConsole.MarkupLine("[red]packages.config found! This will interfere with Chrono![/] Proceed with care.");
-                if (!directoryBuildPropsExist && files.Contains("Directory.Build.props"))
-                {
-                    fileWritePrompt.Select("Directory.Build.props");
-                }
-            }
-
-            if (!directoryBuildPropsExist)
-            {
-                fileWritePrompt.Select("Directory.Build.props");
-            }
-
-            if (!versionFileExist)
-            {
-                fileWritePrompt.Select("version.yml");
-            }
-
-            var filesToWrite = AnsiConsole.Prompt(fileWritePrompt);
-
-            if (filesToWrite.Contains("Directory.Build.props"))
-                File.WriteAllText(directoryBuildPropsPath, InitTemplates.GetBuildProps(settings.AppVersion));
-            if (filesToWrite.Contains("version.yml")) File.WriteAllText(versionFilePath, InitTemplates.GetVersionFile(variant, initialVersion));
+            AnsiConsole.MarkupLine("[red]Aborting[/]");
+            return -1;
         }
 
-        AnsiConsole.MarkupLine("Done!");
+        // === STEP 2. Get Initial Version ===
+        var initialVersion = AnsiConsole.Prompt(new TextPrompt<string>("What is your initial version for the project?").DefaultValue("0.0.1.0"));
+
+        // === STEP 3. Choose a Version Schema Variant ===
+        var variantString = AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+                .Title("Which variant of the version schema do you want to use?")
+                .AddChoices("(local) Minimal", "(local) Default", "(local) Expanded", "(remote inherited) Default", "(remote inherited) Expanded"));
+
+        var variant = variantString switch
+        {
+            "(local) Minimal" => VersionFileVariants.LocalMinimal,
+            "(local) Default" => VersionFileVariants.LocalDefault,
+            "(local) Expanded" => VersionFileVariants.LocalExpanded,
+            "(remote inherited) Default" => VersionFileVariants.InheritedDefault,
+            "(remote inherited) Expanded" => VersionFileVariants.InheritedExpanded,
+            _ => VersionFileVariants.LocalMinimal,
+        };
+
+        // === STEP 4. Choose if Directory.Build.props Should be Created ===
+        var includeBuildProps = AnsiConsole.Confirm("Do you need a Directory.Build.props file to reference Chrono in multiple projects?", !buildPropsExists);
+
+        // === STEP 5. Preview Files to be Written ===
+
+        var versionContent = InitTemplates.GetVersionFile(variant, initialVersion);
+
+        AnsiConsole.Write(new Rule($"[bold]version.yml[/]"));
+        Console.WriteLine(versionContent);
+        AnsiConsole.WriteLine();
+        if (includeBuildProps)
+        {
+            var propsContent = InitTemplates.GetBuildProps(settings.AppVersion);
+            AnsiConsole.Write(new Rule($"[bold]Directory.Build.props[/]"));
+            Console.WriteLine(propsContent);
+            AnsiConsole.WriteLine();
+        }
+
+        // === STEP 6. Confirm & Write Files to Disk ===
+        var writeFiles = AnsiConsole.Confirm("Proceed with writing these files to disk? ([red]Warning:[/] [bold]existing[/] files may be [bold]overwritten![/])", false);
+        if (writeFiles)
+        {
+            File.WriteAllText(Path.Combine(projectDirectory, "version.yml"),
+                InitTemplates.GetVersionFile(variant, initialVersion));
+
+            if (includeBuildProps)
+            {
+                File.WriteAllText(Path.Combine(projectDirectory, "Directory.Build.props"),
+                    InitTemplates.GetBuildProps(settings.AppVersion));
+            }
+
+            AnsiConsole.MarkupLine("[green]Files have been written successfully.[/]");
+        }
+        else
+        {
+            AnsiConsole.MarkupLine("[yellow]Files not written.[/]");
+        }
+
         return 0;
     }
 }
